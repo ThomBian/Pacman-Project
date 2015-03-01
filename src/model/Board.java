4789c2d6f01b534029830ce44839a70b66a134eb
Thomas <thomas.bianchini@ensimag.grenoble-inp.fr>
2015-03-01 15:57:43 +0100

    petit javadoc fix + dat op rapport maj

--- src/model/Board.java
+++ src/model/Board.java
@@ -166,28 +166,13 @@ public class Board {
 
     /* ACCESSEURS ET MODIFIEURS */
 
-    public Content[][] getBoard() {
-        return board;
-    }
-
-    public void setBoard(Content[][] board) {
-        this.board = board;
-    }
-
     public int getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
 
     public int getWidth() {
         return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
     }
 
 	public boolean badNextTile(Tile tile) {
